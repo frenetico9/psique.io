c79e63d20b9d12dc85923b7c944ea24fecc1285d
frenetico9 <hiago182016@gmail.com>
2025-07-22 11:49:32 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import { fileURLToPath } from 'url';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const currentDir = path.dirname(fileURLToPath(import.meta.url));
     return {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -10,7 +12,7 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(currentDir, '.'),
         }
       }
     };
