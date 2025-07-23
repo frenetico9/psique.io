d16a99e564143c0944062ff2ddb27fd74a19ff20
frenetico9 <hiago182016@gmail.com>
2025-07-23 11:19:04 -0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -16,16 +16,3 @@ root.render(
     </AppProvider>
   </React.StrictMode>
 );
-
-// Register Service Worker for PWA
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch(err => {
-        console.log('ServiceWorker registration failed: ', err);
-      });
-  });
-}
